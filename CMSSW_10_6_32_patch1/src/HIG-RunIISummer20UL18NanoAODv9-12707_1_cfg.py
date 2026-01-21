d986ac57741399eb28a949e8df860bf18a753e26
Giovanni Celotto <giovanni.celottog@gmail.com>
2026-01-21 19:03:32 +0100

    working code

--- CMSSW_10_6_32_patch1/src/HIG-RunIISummer20UL18NanoAODv9-12707_1_cfg.py
+++ CMSSW_10_6_32_patch1/src/HIG-RunIISummer20UL18NanoAODv9-12707_1_cfg.py
@@ -3,12 +3,15 @@
 # Revision: 1.19 
 # Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
 # with command line options: --eventcontent NANOEDMAODSIM --customise Configuration/DataProcessing/Utils.addMonitoring --datatier NANOAODSIM --conditions 106X_upgrade2018_realistic_v16_L1v1 --step NANO --era Run2_2018,run2_nanoAOD_106Xv2 --python_filename HIG-RunIISummer20UL18NanoAODv9-12707_1_cfg.py --fileout file:HIG-RunIISummer20UL18NanoAODv9-12707.root --filein dbs:/GluGluHToBB_M-125_TuneCP5_MINLO_NNLOPS_13TeV-powheg-pythia8/RunIISummer20UL18MiniAODv2-106X_upgrade2018_realistic_v16_L1v1-v2/MINIAODSIM --number 1395 --number_out 1395 --no_exec --mc
+
+print("Start")
 import FWCore.ParameterSet.Config as cms
 
 from Configuration.Eras.Era_Run2_2018_cff import Run2_2018
 from Configuration.Eras.Modifier_run2_nanoAOD_106Xv2_cff import run2_nanoAOD_106Xv2
-
+print("Import done")
 process = cms.Process('NANO',Run2_2018,run2_nanoAOD_106Xv2)
+print("Import done2")
 
 # import of standard configurations
 process.load('Configuration.StandardSequences.Services_cff')
@@ -23,10 +26,10 @@ process.load('Configuration.StandardSequences.EndOfProcess_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(1395),
-    output = cms.untracked.int32(1395)
+    input = cms.untracked.int32(30),
+    output = cms.untracked.int32(30)
 )
-
+print("Max events done")
 # Input source
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring( (
@@ -35,9 +38,7 @@ process.source = cms.Source("PoolSource",
     secondaryFileNames = cms.untracked.vstring()
 )
 
-process.options = cms.untracked.PSet(
-
-)
+process.options = cms.untracked.PSet()
 
 # Production Info
 process.configurationMetadata = cms.untracked.PSet(
@@ -74,24 +75,60 @@ unpackedTracksAndVertices = cms.EDProducer('PATTrackAndVertexUnpacker',
 )
 
 process.load("RecoVertex.AdaptiveVertexFinder.inclusiveVertexing_cff")
+
 process.unpackedTracksAndVertices = unpackedTracksAndVertices
-process.inclusiveCandidateVertexFinder.tracks = cms.InputTag(     "unpackedTracksAndVertices" )
+
+process.inclusiveCandidateVertexFinder.tracks = cms.InputTag(     "packedPFCandidates" ) #https://github.com/cms-sw/cmssw/blob/712e925576cdaabfc2bc3abfb2162b38756f09ee/PhysicsTools/PatAlgos/plugins/TrackAndVertexUnpacker.cc#L54
 process.inclusiveCandidateVertexFinder.primaryVertices = cms.InputTag(     "unpackedTracksAndVertices" )
+
 process.candidateVertexMerger.secondaryVertices = cms.InputTag(     "inclusiveCandidateVertexFinder" )
-process.candidateVertexArbitrator.tracks = cms.InputTag(     "unpackedTracksAndVertices" )
+
+process.candidateVertexArbitrator.tracks = cms.InputTag(     "packedPFCandidates" )
 process.candidateVertexArbitrator.primaryVertices = cms.InputTag(     "unpackedTracksAndVertices" )
 process.candidateVertexArbitrator.secondaryVertices = cms.InputTag(     "candidateVertexMerger" )
+
 process.inclusiveCandidateSecondaryVertices.secondaryVertices = cms.InputTag(     "candidateVertexArbitrator" )
+
+
+
+
+from PhysicsTools.NanoAOD.common_cff import Var, CandVars
+process.svAVFTable = cms.EDProducer(
+    "SimpleCompositeCandidateFlatTableProducer",
+    src = cms.InputTag("inclusiveCandidateSecondaryVertices"),  # your SV collection
+    cut = cms.string(""),  # can filter SVs if needed
+    name = cms.string("SVAVF"),  # this will be the branch prefix
+    doc  = cms.string("Secondary vertices from AVF"),
+    singleton = cms.bool(False),   # we have multiple SVs per event
+    extension = cms.bool(False),   # not adding to existing table
+    variables = cms.PSet(
+        x        = Var("position().x()", float, doc="x coordinate of SV"),
+        y        = Var("position().y()", float, doc="y coordinate of SV"),
+        z        = Var("position().z()", float, doc="z coordinate of SV"),
+        #chi2     = Var("chi2()", float, doc="chi2 of SV fit"),
+        #ndof     = Var("ndof()", float, doc="ndof of SV fit"),
+        nTracks  = Var("numberOfDaughters()", int, doc="number of tracks in SV"),
+        #mass     = Var("p4().mass()", float, doc="mass of SV candidate")
+    )
+)
+
+
+
+
+
 
 process.nanoAOD_step = cms.Path(
     process.unpackedTracksAndVertices *
-    #process.inclusiveCandidateVertexFinder *
-    #process.candidateVertexMerger *
-    #process.candidateVertexArbitrator *
-    #process.inclusiveCandidateSecondaryVertices *
+    process.inclusiveCandidateVertexFinder *
+    process.candidateVertexMerger *
+    process.candidateVertexArbitrator *
+    process.inclusiveCandidateSecondaryVertices *
     process.nanoSequenceMC
 )
-
+process.NANOEDMAODSIMoutput.outputCommands += [
+    'drop *',
+    "keep nanoaodFlatTable_*Table*_*_*",  # Keep event-level FlatTables
+]
 process.endjob_step = cms.EndPath(process.endOfProcess)
 process.NANOEDMAODSIMoutput_step = cms.EndPath(process.NANOEDMAODSIMoutput)
 
